29821d2a1e92f45ab89455a302d4701deb02fd5f
Ken Hoff <ken@hoff.tech>
2016-04-05 14:26:35 -0600

    v1

--- tsa-randomizer.js
+++ tsa-randomizer.js
@@ -1,36 +1,10 @@
-$(".rightArrow").animate({
-	opacity: 0
-}, 0)
-$(".leftArrow").animate({
-	opacity: 0
-}, 0)
-
-var isAnimating
-
 $(".randomizeButton").click(function() {
-	if (!isAnimating) {
-		isAnimating = true;
-		$(".rightArrow").animate({
-			opacity: 0
-		}, 1000)
-		$(".leftArrow").animate({
-			opacity: 0
-		}, 1000)
-
-		// reveal either the left or the right arrow
-		randomNumber = Math.random()
-		if (randomNumber > 0.5) {
-			$(".rightArrow").animate({
-				opacity: 1
-			}, 1000, function() {
-				isAnimating = false
-			})
-		} else {
-			$(".leftArrow").animate({
-				opacity: 1
-			}, 1000, function() {
-				isAnimating = false
-			})
-		}
+	$(".arrows > *").addClass("invisible").removeClass("leftArrowAnimate rightArrowAnimate")
+	if (Math.random() >= 0.5) {
+		$(".leftArrow").clone().removeClass("invisible").insertAfter(".leftArrow");
+		$(".leftArrow:first").remove();
+	} else {
+		$(".rightArrow").clone().removeClass("invisible").insertAfter(".rightArrow");
+		$(".rightArrow:first").remove();
 	}
 })
